038303e28658093b3438baaba50629a9e0c30c6c
caiopessoa-dev <caiosantos1201@gmail.com>
2025-10-22 16:19:02 -0300

    Update NoRubroNegra.java

--- src/NoRubroNegra.java
+++ src/NoRubroNegra.java
@@ -1,11 +1,11 @@
 public class NoRubroNegra {
     int chave;
     NoRubroNegra esquerda, direita, pai;
-    boolean cor; // true = vermelho, false = preto
+    boolean cor; 
 
     NoRubroNegra(int chave) {
         this.chave = chave;
-        this.cor = true; // novos nós sempre iniciam como vermelhos
+        this.cor = true;
     }
 }
 
